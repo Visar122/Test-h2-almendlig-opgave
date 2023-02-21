13627be787387a6271cc1e39dcd7f4f2c1be2506
shaba <shaba@DESKTOP-OM16O0D>
2023-02-21 16:40:27 +0100

    Add project files.

---
+++ Test h2 almendlig opgave/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_h2_almendlig_opgave
+{
+public class Class1
+	{
+		public int amount;
+
+		public Class1()
+		{
+			amount= 2;
+		}
+		public void Add2()
+		{
+			amount+=2;
+		}
+	}
+}
---
+++ Test h2 almendlig opgave/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Test_h2_almendlig_opgave;
+
+namespace Testh2almendligopgave
+{
+	public class program
+	{
+		static void Main(string[] args)
+		{
+			// Erklærer variablen "amount" af typen int og tildere den samtidig værdin 2 
+			 // Må ikke ændres i iopgavens løsning.
+	         Class1 amount1= new Class1();	
+			 amount1.Add2();
+
+			Console.WriteLine($"Nuværende værdi af amount {amount1.amount}");
+			Console.ReadLine();
+		}
+	
+		// Opgaven 
+		// Du skal ændre  i programmet så kaldet til Console.WriteLine på linie 14 resultater i værdien 4 på grun af kaldt til metoden Add2
+		// Dum må godt lave ekstra kode,altså flere linjer i fx. metoden Add2. Siger ikke om det vil være en løsning
+	}
+}
